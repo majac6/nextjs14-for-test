2ff55177b777bb537fcbbf8175e0aff409ccf25c
HungSun LIM <majac6@gmail.com>
2024-07-03 11:45:59 +0900

    한번더

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            코드 바꿈
+            코드 한번더 바꿈
           </p>
         </a>
       </div>
